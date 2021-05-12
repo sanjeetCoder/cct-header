018f3825734f16777ce101a0219eb6966e3d1bc9
Sanjeet Kumar <sanjeetcoders@gmail.com>
2021-05-12 13:49:05 +0530

    Adding a Library

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
-import { ExampleComponent } from 'cct-header'
-import 'cct-header/dist/index.css'
-
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <table class='table table-dark'>
+        <thead>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>First</th>
+            <th scope='col'>Last</th>
+            <th scope='col'>Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope='row'>2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope='row'>3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default App
